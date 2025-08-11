59b468f87600ecd5c38ba3d4a6e9378cea3277f6
Gatsby01k <vladyslavddos@gmail.com>
2025-08-11 21:07:44 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,13 @@
 import React from "react";
-import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom/client";
 import App from "./App";
+import { WalletProviders } from "./wallet";
 import "./styles.css";
 
-const root = createRoot(document.getElementById("root")!);
-root.render(<App />);
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <WalletProviders>
+      <App />
+    </WalletProviders>
+  </React.StrictMode>
+);
