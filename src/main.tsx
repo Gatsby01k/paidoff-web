37091ce40638d91a72da78855fb5c115235ebd3b
Gatsby01k <vladyslavddos@gmail.com>
2025-08-13 21:29:41 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,9 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import { createRoot } from "react-dom/client";
 import App from "./App";
-import AppSafe from "./AppSafe";
-import { WalletProviders } from "./wallet";
-import { SAFE } from "./safe";
-import "./styles.css";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    {SAFE ? (
-      <AppSafe />
-    ) : (
-      <WalletProviders>
-        <App />
-      </WalletProviders>
-    )}
+    <App />
   </React.StrictMode>
 );
